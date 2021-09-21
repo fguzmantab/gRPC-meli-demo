8409a497a6fd8e8d14eabc5732e5052be1273bcf
Freddy Guzman <freddy.guzman@mercadolibre.cl>
2021-09-21 07:37:16 -0300

    Added server streaming and bidi streaming

--- client/client.go
+++ client/client.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io"
 	"log"
+	"time"
 
 	"google.golang.org/grpc"
 
@@ -20,11 +21,18 @@ func main() {
 
 	c := item_proto.NewItemServiceClient(conn)
 
+	fmt.Printf("\n\n###################################\n")
 	getItem(c)
-	getItemBySellerId(c)
+	fmt.Printf("\n\n###################################\n")
+	getItemBySellerID(c)
+	fmt.Printf("\n\n###################################\n")
+	createItems(c)
+	fmt.Printf("\n\n###################################\n")
+	bidi(c)
 }
 
 func getItem(c item_proto.ItemServiceClient) {
+	fmt.Printf("Starting to do a Unary RPC\n\n")
 	itemId := &item_proto.ItemId{Id: "MLA123"}
 
 	res, err := c.Get(context.Background(), itemId)
@@ -34,8 +42,8 @@ func getItem(c item_proto.ItemServiceClient) {
 	log.Printf("Item: %v\n", res)
 }
 
-func getItemBySellerId(c item_proto.ItemServiceClient) {
-	fmt.Println("Starting to do a Server Streaming RPC")
+func getItemBySellerID(c item_proto.ItemServiceClient) {
+	fmt.Printf("Starting to do a Server Streaming RPC\n\n")
 
 	sellerID := &item_proto.SellerId{Id: 11111}
 	stream, err := c.GetItemsBySeller(context.Background(), sellerID)
@@ -55,4 +63,95 @@ func getItemBySellerId(c item_proto.ItemServiceClient) {
 
 		fmt.Printf("Item %v\n", msg)
 	}
+}
+
+func createItems(c item_proto.ItemServiceClient) {
+	fmt.Printf("Starting to do a Client Streaming RPC\n\n")
+
+	items := []*item_proto.Item{
+		{
+			Id:    "MLA1234",
+			Title: "title 1",
+		},
+		{
+			Id:    "MLA5678",
+			Title: "title 2",
+		},
+		{
+			Id:    "MLA8888",
+			Title: "title 3",
+		},
+		{
+			Id:    "MLA6666",
+			Title: "title 4",
+		},
+	}
+
+	stream, err := c.CreateItems(context.Background())
+	if err != nil {
+		log.Panicf("error while calling CreateItems: %v", err)
+	}
+
+	for _, req := range items {
+		fmt.Printf("Sending item:%v\n", req)
+		stream.Send(req)
+		time.Sleep(time.Second)
+	}
+
+	response, err := stream.CloseAndRecv()
+	if err != nil {
+		log.Panicf("error while recieving response from CreateItems: %v", err)
+	}
+
+	fmt.Printf("\nItem ids created: %v\n", response)
+}
+
+func bidi(c item_proto.ItemServiceClient) {
+	fmt.Println("Starting to do a BiDi Streaming RPC")
+
+	requests := []*item_proto.ItemId{
+		{
+			Id: "MLA1234",
+		},
+		{
+			Id: "MLA3333",
+		},
+		{
+			Id: "MLA4444",
+		},
+		{
+			Id: "MLA5555",
+		},
+	}
+
+	stream, err := c.BidiItems(context.Background())
+	if err != nil {
+		log.Panicf("error while calling BiDI stream: %v", err)
+	}
+
+	waitc := make(chan struct{})
+	go func() {
+		for _, req := range requests {
+			fmt.Printf("Sending item id:%v\n", req)
+			stream.Send(req)
+			time.Sleep(time.Second)
+		}
+		stream.CloseSend()
+	}()
+
+	go func() {
+		for {
+			response, err := stream.Recv()
+			if err == io.EOF {
+				break
+			}
+			if err != nil {
+				log.Fatalf("Error while receiving: %v\n", err)
+			}
+			fmt.Printf("Received: %v\n", response.GetId())
+		}
+		close(waitc)
+	}()
+
+	<-waitc
 }
--- server/server.go
+++ server/server.go
@@ -3,8 +3,10 @@ package main
 import (
 	"context"
 	"fmt"
+	"io"
 	"log"
 	"net"
+	"time"
 
 	"google.golang.org/grpc"
 
@@ -15,6 +17,7 @@ type server struct {
 	item_proto.UnimplementedItemServiceServer
 }
 
+// Get Unary
 func (s *server) Get(ctx context.Context, itemId *item_proto.ItemId) (*item_proto.Item, error) {
 	fmt.Printf("Getting item %s", itemId.GetId())
 	item := &item_proto.Item{
@@ -42,6 +45,7 @@ func (s *server) Get(ctx context.Context, itemId *item_proto.ItemId) (*item_prot
 	return item, nil
 }
 
+// GetItemsBySeller Server streaming
 func (s server) GetItemsBySeller(sellerID *item_proto.SellerId, stream item_proto.ItemService_GetItemsBySellerServer) error {
 	fmt.Printf("Calling stream server with %s\n", sellerID.GetId())
 
@@ -56,9 +60,59 @@ func (s server) GetItemsBySeller(sellerID *item_proto.SellerId, stream item_prot
 		}
 
 		stream.Send(item)
+		time.Sleep(time.Second)
 	}
 
 	return nil
+}
+
+// CreateItems Client streaming
+func (s server) CreateItems(stream item_proto.ItemService_CreateItemsServer) error {
+	fmt.Printf("Calling stream client\n")
+	var ids []string
+
+	for {
+		req, err := stream.Recv()
+		if err == io.EOF {
+			var itemIDs []*item_proto.ItemId
+			for _, id := range ids {
+				itemIDs = append(itemIDs, &item_proto.ItemId{Id: id})
+			}
+			return stream.SendAndClose(&item_proto.ItemIds{Ids: itemIDs})
+		}
+
+		if err != nil {
+			log.Panicf("Error while reading client stream: %v", err)
+		}
+
+		ids = append(ids, req.GetId())
+	}
+
+	return nil
+}
+
+// BidiItems Bidirectional streaming
+func (s server) BidiItems(stream item_proto.ItemService_BidiItemsServer) error {
+	fmt.Printf("BidiItems function was invoked with a bi streaming request\n")
+
+	for {
+		req, err := stream.Recv()
+		if err == io.EOF {
+			return nil
+		}
+
+		if err != nil {
+			log.Panicf("Error while reading client stream: %v", err)
+			return err
+		}
+
+		req.GetId()
+		err = stream.Send(&item_proto.Item{Id: req.GetId(), Title: "Title: " + req.GetId()})
+		if err != nil {
+			log.Fatalf("Error while sending data to client: %v", err)
+			return err
+		}
+	}
 }
 
 func main() {
